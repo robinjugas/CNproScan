db142623a6da5fe54aab9e03353237e089fa9678
robinjugas <robinjugas@gmail.com>
2022-11-20 15:59:56 +0100

    corrected GCnormalization giving Inf values

--- R/gcNormalization.R
+++ R/gcNormalization.R
@@ -18,13 +18,11 @@ gcNormalization <- function(coverageVECTOR, fastaFile, refHeader){
   GC_count <- rep(0, times=referenceLength)
   coverageVECTOR_new <- rep(0, times=referenceLength)
   med <- median(coverageVECTOR)
+  avg <- mean(coverageVECTOR)
   
   ##  READ FASTA FILE
   reference <- seqinr::read.fasta(fastaFile, seqonly = FALSE, whole.header=FALSE)
-  # refHeader %in% names(reference) 
-  # which(names(reference)==refHeader)
   reference <- reference[[which(names(reference)==refHeader)]]
-  
   
   ## Calculate GC content in window
   for (i in seq(from=1, to=referenceLength, by=windowLength/2)){
@@ -38,28 +36,44 @@ gcNormalization <- function(coverageVECTOR, fastaFile, refHeader){
   GC_count <- GC_count*100 #make a percentage
   
   ## GC dataframe GC_tab - 1.col - GC value in % 2.col - median coverageVECTOR of all windows with given GC % value
-  GC_tab <- data.frame(GC=integer(), MED_COV=integer())
+  GC_tab <- data.frame(GC=integer(), MED_COV=integer(),AVG_COV=integer(),PARAMETER=integer(),PARAMETER2=integer())
   GCsek <- seq(from=0, to=100)
   GC_tab[1:length(GCsek), "GC"] <- GCsek
   GC_tab[, "MED_COV"] <- rep(0, times=length(GCsek))
   for(i in 1:nrow(GC_tab)){
     position <- which(GC_count==GC_tab[i, "GC"])
-    pom <- median(coverageVECTOR[position])
-    GC_tab[i, "MED_COV"]  <- pom
+    GC_tab[i, "MED_COV"]  <- median(coverageVECTOR[position])
+    GC_tab[i, "AVG_COV"]  <- mean(coverageVECTOR[position])
+    GC_tab[i, "PARAMETER"]  <- med/GC_tab[i, "MED_COV"]
+    GC_tab[i, "PARAMETER2"]  <- med/GC_tab[i, "AVG_COV"]
   }
   
-  ## New normalized coverage
+  ## New normalized coverage - what if MEDIAN COVERAGE IS ZERO? REPLACE BY AVERAGE OR DO NOTHING
   for(i in 1:referenceLength){
     GC_act <- as.integer(GC_count[i])
-    if( GC_act == 0 | GC_tab[which(GC_tab$GC==GC_act), "GC"] == 0 | is.na(GC_tab[which(GC_tab$GC==GC_act), "MED_COV"]) | coverageVECTOR[i] < 1  ){ 
-      coverageVECTOR_new[i] <- coverageVECTOR[i] # zustava puvodni pokryti
-    }else{
-      med2 <- GC_tab[which(GC_tab$GC==GC_act), "MED_COV"] # vyhledani medianu pozic pokryti s danym GC poctem ve vytvorene tabulce
+    if( GC_act == 0 | GC_tab[which(GC_tab$GC==GC_act), "GC"] == 0 | is.na(GC_tab[which(GC_tab$GC==GC_act), "MED_COV"]) | coverageVECTOR[i] < 1 ){ 
+      coverageVECTOR_new[i] <- coverageVECTOR[i] # keep unormalized
+      
+    }else if(GC_tab[which(GC_tab$GC==GC_act), "MED_COV"] == 0 & GC_tab[which(GC_tab$GC==GC_act), "AVG_COV"] == 0){
+      coverageVECTOR_new[i] <- coverageVECTOR[i] # keep unormalized
+      
+    }else if(GC_tab[which(GC_tab$GC==GC_act), "MED_COV"] <= 1 & GC_tab[which(GC_tab$GC==GC_act), "AVG_COV"] != 0){
+      avg2 <- GC_tab[which(GC_tab$GC==GC_act), "AVG_COV"] # replace median by average of RD of that GC%
+      GC_tab[which(GC_tab$GC==GC_act), "AVG_COV"]
+      coverageVECTOR_new[i] <- coverageVECTOR[i]*(med/avg2)
+      
+    } else{
+      med2 <- GC_tab[which(GC_tab$GC==GC_act), "MED_COV"] # median of RD of that GC%
+      GC_tab[which(GC_tab$GC==GC_act), "MED_COV"] 
       coverageVECTOR_new[i] <- coverageVECTOR[i]*(med/med2) 
+      
     }
   }
   
   # ##COMPARISON
+  # summary(coverageVECTOR)
+  # summary(coverageVECTOR_new)
+  # 
   # diff <- abs(coverageVECTOR_new-coverageVECTOR)
   # plot(diff)
   # #original dependence
@@ -76,6 +90,7 @@ gcNormalization <- function(coverageVECTOR, fastaFile, refHeader){
   # }
   # #new dependence
   # plot(x=GC_tab$GC,y=GC_tab$MED_COV)
+  # # plot genomes
   # plot(coverageVECTOR)
   # plot(coverageVECTOR_new)
   
--- R/oricNormalization.R
+++ R/oricNormalization.R
@@ -1,60 +1,89 @@
 #' REPLICATION ORIGIN BIAS normalization
-#' This function normalizes the replication origin bias. 
+#' This function normalizes the replication origin bias. Inspired by https://github.com/XiDsLab/CNV-BAC
 #' 
 #' @importFrom mgcv gam predict.gam
 #' @param coverage dataframe of coverage, 3 columns CHROM POS COVERAGE
-#' @param oriC_position position of oriC in the reference genome
+#' @param referenceLength
+#' @param oriC_position position of oriC in the reference genomem can be one or multiple
 #' @return A vector of normalized coverage values
 #' @noRd
 #' 
-oricNormalization <- function(coverage, oriC_position=1){
-  ## 
-  referenceLength <- length(coverage)
-  coverage_new <- rep(0, times=referenceLength)
-  RC_count <- data.frame(READEPTH=integer(),oriDISTANCE=integer())
+oricNormalization <- function(coverageVector, referenceLength, oriC_position){
+  
+  ## oriC position
+  if(is.vector(oriC_position)){numOriC <- length(oriC_position)}else{numOriC <- length(oriC_position)}
+  
+  ## variables
+  coverage_new <- rep(0, times=referenceLength) #prepare output vector
+  RC_count <- data.frame(READEPTH=integer(),POS=integer(), oriDISTANCE=integer()) #reconstruct DF
+  
+  ################################################################################
   ## GET DISTANCE from ORIC
-  RC_count[1:referenceLength,"READEPTH"] <- coverage
-  RC_count[,"oriDISTANCE"] <- seq(from = 1, to = referenceLength)
-  RC_count[,"oriDISTANCE"] <- abs(RC_count[,"oriDISTANCE"] - oriC_position) #distance from ORIC1
-  # RC_count[,"oriDISTANCE"] <- referenceLength - RC_count[,"oriDISTANCE"] + oriC_position #distance from ORIC2
-  # RC_count[,"oriDISTANCE"] <- referenceLength + RC_count[,"oriDISTANCE"] - oriC_position #distance from ORIC3
+  RC_count[1:referenceLength,"POS"] <- seq(from = 1, to = referenceLength)
+  RC_count[1:referenceLength,"READEPTH"] <- coverageVector
   
+  # Circular genome correction and multiple oriC handling
+  for(oriC in oriC_position){
+    RC_count <- cbind(RC_count, abs(RC_count$POS-oriC))
+    RC_count <- cbind(RC_count, abs(referenceLength - RC_count$POS + oriC))
+    RC_count <- cbind(RC_count, referenceLength + RC_count$POS - oriC)
+  }
+  RC_count[,"oriDISTANCE"] <- apply(RC_count[,c(4:ncol(RC_count))],1,min)
+  # positions in between two most distant oriC's are zeroed
+  index <- intersect(which(RC_count$POS <= max(oriC)),which(RC_count$POS >= min(oriC)))
+  RC_count[index,"oriDISTANCE"] <- 0
+  
+  #
+  RC_count <- RC_count[,c(1:3)]
+  
+  # plot(RC_count$oriDISTANCE)
+  
+  ################################################################################
+
   ## BOOSTRAPING and PLOTTING
-  # XX = RC_count[sample(nrow(RC_count), 50000, replace = FALSE), ]
-  # library(ggplot2)
-  # ggplot(aes(x=READEPTH, y=oriDISTANCE),data=XX) + geom_point()
+  XX <- RC_count[sample(nrow(RC_count), 50000, replace = FALSE), ]
+  XX <- XX[order(XX$oriDISTANCE),]
+  XX <- XX[XX$READEPTH<summary(abs(RC_count$READEPTH))[[5]],] #remove values behind 3rd quantile
+  library(ggplot2)
+  ggplot(aes(x=oriDISTANCE, y=READEPTH),data=XX) + geom_point()
   
-  # summary(RC_count[,"oriDISTANCE"])
-  # summary(RC_count[,"READEPTH"])
+  summary(RC_count[,"oriDISTANCE"])
+  summary(RC_count[,"READEPTH"])
+  
+  ################################################################################
   
   ## CORRELATION BETWEEN DISTANCE and COVERAGE
-  statistics <- cor.test(RC_count[,"oriDISTANCE"],RC_count[,"READEPTH"],method ="spearman",exact=FALSE)
+  statistics <- cor.test(RC_count[,"oriDISTANCE"], RC_count[,"READEPTH"], method ="spearman", exact=FALSE)
+  statistics$p.value
+  statistics$estimate
   RHO <- statistics$estimate
-  if (RHO >= 0.9){
+  if (RHO >= 0.9){ #maybe different value
     # ## REPLACE OUTLIERS
-    # readdepthmedian <- median(abs(RC_count$READEPTH))
-    # readdepth_1Q <- summary(abs(RC_count$READEPTH))[[2]]
-    # readdepth_3Q <- summary(abs(RC_count$READEPTH))[[5]]
-    # readdepth_IQR <- IQR(abs(RC_count$READEPTH))
-    # #find outliers 1.5xIQR rule
-    # out_ind <- which(abs(RC_count$READEPTH)<(readdepth_3Q+1.5*readdepth_IQR) & abs(RC_count$READEPTH)>(readdepth_1Q-1.5*readdepth_IQR) )
-    # # replace outliers value with MEDIAN
-    # RC_count[out_ind,"READEPTH"] <- readdepthmedian
+    readdepthmedian <- median(abs(RC_count$READEPTH))
+    readdepth_1Q <- summary(abs(RC_count$READEPTH))[[2]]
+    readdepth_3Q <- summary(abs(RC_count$READEPTH))[[5]]
+    readdepth_IQR <- IQR(abs(RC_count$READEPTH))
+    #find outliers 1.5xIQR rule
+    out_ind <- which(abs(RC_count$READEPTH)<(readdepth_3Q+1.5*readdepth_IQR) & abs(RC_count$READEPTH)>(readdepth_1Q-1.5*readdepth_IQR) )
+    # replace outliers value with MEDIAN
+    RC_count[out_ind,"READEPTH"] <- readdepthmedian
     # 
     # fit model with read-depth without outliers
-    RC <- RC_count$READEPTH
-    DIST <- RC_count$oriDISTANCE
-    fit <- mgcv::gam(RC~s(DIST))
-    RC <- as.data.frame(coverage)
+    RC <- as.vector(RC_count$READEPTH)
+    DIST <- as.vector(RC_count$oriDISTANCE)
+    DIST <- scale(DIST)
+    RC <- log2(RC)
+    fit <- mgcv::bam(RC~s(DIST))
+    RC <- as.data.frame(coverageVector)
     pred <- mgcv::predict.gam(fit,RC)
     expected <- RC_count$READEPTH + pred
     # RC_count$normalized <- expected
-    coverage_new <- expected
+    coverageVector_new <- expected
     
-    return(coverage_new)
+    return(coverageVector_new)
   }
   else{
-    return(coverage)
+    return(coverageVector)
   }
 }
 
